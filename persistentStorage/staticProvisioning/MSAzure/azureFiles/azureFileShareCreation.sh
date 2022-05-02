29c4d68a56b1b4dcb4b8b101d5be5cba159ce0c0
Tashik <k180142@nu.edu.pk>
2022-05-02 09:05:23 +0500

    added persistent storage resources

---
+++ persistentStorage/staticProvisioning/MSAzure/azureFiles/azureFileShareCreation.sh
@@ -0,0 +1,28 @@
+# Change these four parameters as needed for your own environment
+export AKS_PERS_STORAGE_ACCOUNT_NAME=mystorageaccount$RANDOM
+export AKS_PERS_RESOURCE_GROUP=myaksfilesharerg
+export AKS_PERS_LOCATION=eastus
+export AKS_PERS_SHARE_NAME=aks-file-share
+
+# Create a resource group
+az group create --name $AKS_PERS_RESOURCE_GROUP --location $AKS_PERS_LOCATION
+
+# Create a storage account
+az storage account create -n $AKS_PERS_STORAGE_ACCOUNT_NAME -g $AKS_PERS_RESOURCE_GROUP -l $AKS_PERS_LOCATION --sku Standard_LRS
+
+# Export the connection string as an environment variable, this is used when creating the Azure file share
+export AZURE_STORAGE_CONNECTION_STRING=$(az storage account show-connection-string -n $AKS_PERS_STORAGE_ACCOUNT_NAME -g $AKS_PERS_RESOURCE_GROUP -o tsv)
+
+# Create the file share
+az storage share create -n $AKS_PERS_SHARE_NAME --connection-string $AZURE_STORAGE_CONNECTION_STRING
+
+# Get storage account key
+STORAGE_KEY=$(az storage account keys list --resource-group $AKS_PERS_RESOURCE_GROUP --account-name $AKS_PERS_STORAGE_ACCOUNT_NAME --query "[0].value" -o tsv)
+
+# Echo storage account name and key
+echo Storage account name: $AKS_PERS_STORAGE_ACCOUNT_NAME
+echo Storage account key: $STORAGE_KEY
+
+
+
+kubectl create secret generic azure-secret --from-literal=azurestorageaccountname=$AKS_PERS_STORAGE_ACCOUNT_NAME --from-literal=azurestorageaccountkey=$STORAGE_KEY
